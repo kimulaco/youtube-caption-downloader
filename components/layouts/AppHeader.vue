848888c9c5dda387aec660a31cb6897d21b88f77
kimulaco <kmrkdev@gmail.com>
2019-02-28 21:40:16 +0900

    fixed layouts

--- components/layouts/AppHeader.vue
+++ components/layouts/AppHeader.vue
@@ -5,12 +5,14 @@
       role="navigation"
       aria-label="main navigation"
     >
-      <h1 class="navbar-brand">
-        <router-link
-          class="navbar-item"
-          to="/"
-        >YouTube Caption Downloader</router-link>
-      </h1>
+      <div class="inner">
+        <h1 class="navbar-brand">
+          <router-link
+            class="navbar-item"
+            to="/"
+          >YouTube Caption Downloader</router-link>
+        </h1>
+      </div>
     </nav>
   </header><!-- /.app-header -->
 </template>
@@ -23,4 +25,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.app-header {
+  .inner {
+    width: 100%;
+    max-width: 960px;
+    margin: 0 auto;
+  }
+}
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,27 +2,9 @@
   <div>
     <app-header />
 
-    <section class="main-content columns">
-      <aside class="column is-2 section">
-        <ul class="menu-list">
-          <li
-            v-for="(item, key) of items"
-            :key="key"
-          >
-            <nuxt-link
-              :to="item.to"
-              exact-active-class="is-active"
-            >
-              <b-icon :icon="item.icon" /> {{ item.title }}
-            </nuxt-link>
-          </li>
-        </ul>
-      </aside>
-
-      <div class="container column is-10">
-        <nuxt />
-      </div>
-    </section>
+    <main class="container">
+      <nuxt />
+    </main>
   </div>
 </template>
 
